a02d890cab77945ee4e37ad9c63404fb3a14f3cb
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-02 21:45:14 +0530

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# snake-game
-Traditional Snake Game in ReactJs
---
+++ precache-manifest.97b8129a64c74f1fea41a6122c179c27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "737fd59bbb13fcbe2a7c6914034a4649",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7a655fcc8aedf3638fe",
+    "url": "/static/css/main.987d6733.chunk.css"
+  },
+  {
+    "revision": "55139f1345cc03044468",
+    "url": "/static/js/2.93469996.chunk.js"
+  },
+  {
+    "revision": "e7a655fcc8aedf3638fe",
+    "url": "/static/js/main.2f16ad79.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6786807550dc3695a866e2ccbf8b5c32",
+    "url": "/static/media/alchemy.67868075.gif"
+  }
+]);
